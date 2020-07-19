0920603503fb8bbe3f8a3959781142bcc955d913
Jacob Sides <jsides@pyxl.com>
2020-07-19 13:58:30 -0400

    final

--- src/scripts/client.js
+++ src/scripts/client.js
@@ -2,9 +2,20 @@ import './gmap';
 import { db, getPins } from './db';
 import { attachHandlers, createPins } from './ui';
 
+window.markers = [];
 getPins()
     .then(pins => {
         createPins(pins);
+        const feed = [...document.querySelectorAll('.post')]
+        feed.forEach((post, ind) => {
+                // console.log(post)
+            post.addEventListener('hover', () => {
+                if (markers[ind].getAnimation() !== null) {
+                    markers[ind].setAnimation(google.maps.Animation.BOUNCE);
+                }
+            })
+        })
+
         // createFeed(pins);
     })
 
